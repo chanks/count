6e7506ab306dfe82fd5b0e710efe5f051f5bca7a
Chris Hanks <christopher.m.hanks@gmail.com>
2010-12-30 18:46:51 -0800

    For boolean specs, ensure that both true/false are seen across multiple requests.

--- spec/integration/boolean_controller_spec.rb
+++ spec/integration/boolean_controller_spec.rb
@@ -1,11 +1,13 @@
 require 'spec_helper'
 
 describe "A controller action with a boolean a/b test" do
-  before do
+  def result
     get '/booleans/test'
+    last_response.body
   end
 
-  it "should return true or false, not both" do
-    %w(true false).should include last_response.body
+  it "should return true or false for different users" do
+    results = (1..10).map { result }
+    results.uniq.sort.should == %w(false true)
   end
 end
--- spec/integration/boolean_view_spec.rb
+++ spec/integration/boolean_view_spec.rb
@@ -1,11 +1,13 @@
 require 'spec_helper'
 
 describe "A user visiting a page with a boolean a/b test" do
-  before do
+  def result
     get '/view_tests/test/boolean'
+    last_response.body.strip
   end
 
-  it "should see either true or false, not both" do
-    %w(true false).should include last_response.body.strip
+  it "should see true or false for different users" do
+    results = (1..10).map { result }
+    results.uniq.sort.should == %w(false true)
   end
 end
