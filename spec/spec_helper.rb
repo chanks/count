3c1be9912f869de8597b3bf7e442827d17d41434
Chris Hanks <christopher.m.hanks@gmail.com>
2010-12-31 13:01:04 -0800

    It would help a lot if the spec helpers were actually loaded when the specs run. Sigh.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,10 @@ ENV["RAILS_ENV"] = "test"
 
 require 'rails_app/config/environment'
 
+# Requires supporting files with custom matchers and macros, etc,
+# in ./support/ and its subdirectories.
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each(&method(:require))
+
 include Rack::Test::Methods
 
 def app
