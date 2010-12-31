f8cc884f392cb0e1f8c4c41b2f34168203103cef
Chris Hanks <christopher.m.hanks@gmail.com>
2010-12-30 22:13:05 -0800

    Spec that boolean a/b testing returns true and false each about half the time.

--- spec/integration/boolean_controller_spec.rb
+++ spec/integration/boolean_controller_spec.rb
@@ -6,9 +6,12 @@ describe "A controller action with a boolean a/b test" do
     last_response.body
   end
 
-  it "should return true or false for different users" do
-    results = (1..10).map { clear_cookies; result }
-    results.uniq.sort.should == %w(false true)
+  it "should return true and false each approximately 50% of the time" do
+    results = (1..100).map { clear_cookies; result }
+
+    %w(true false).each do |possibility|
+      results.select{|s| s == possibility}.count.should be_within(10).of(50)
+    end
   end
 
   it "should always return either true or false for a given user" do
--- spec/integration/boolean_view_spec.rb
+++ spec/integration/boolean_view_spec.rb
@@ -6,9 +6,12 @@ describe "A user visiting a page with a boolean a/b test" do
     last_response.body.strip
   end
 
-  it "should see true or false for different users" do
-    results = (1..10).map { clear_cookies; result }
-    results.uniq.sort.should == %w(false true)
+  it "should return true and false each approximately 50% of the time" do
+    results = (1..100).map { clear_cookies; result }
+
+    %w(true false).each do |possibility|
+      results.select{|s| s == possibility}.count.should be_within(10).of(50)
+    end
   end
 
   it "should always see either true or false for a given user" do
