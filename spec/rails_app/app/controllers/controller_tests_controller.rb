36122a0692fa1ed876afbf747bf8e64e3a3242ba
Chris Hanks <christopher.m.hanks@gmail.com>
2011-01-02 14:45:47 -0800

    Clean up ControllerTestsController a bit.

--- spec/rails_app/app/controllers/controller_tests_controller.rb
+++ spec/rails_app/app/controllers/controller_tests_controller.rb
@@ -1,8 +1,10 @@
 class ControllerTestsController < ApplicationController
 
+
   # ab_test specs.
+
   def array
-    render :text => ab_test('array', [1, 2, 3]).to_s
+    render :text => ab_test('array', [1, 2, 3])
   end
 
   def array_bingo
@@ -10,64 +12,50 @@ class ControllerTestsController < ApplicationController
     render :nothing => true
   end
 
-  def boolean
-    render :text => ab_test('boolean').to_s
-  end
 
-  def boolean_bingo
-    bingo! 'boolean'
-    render :nothing => true
+  # ab_test alternatives specs.
+
+  def boolean
+    render :text => ab_test('boolean')
   end
 
   def hash
-    render :text => ab_test('hash', {1 => 2, 2 => 1}).to_s
-  end
-
-  def hash_bingo
-    bingo! 'hash'
-    render :nothing => true
+    render :text => ab_test('hash', {1 => 2, 2 => 1})
   end
 
   def integer
-    render :text => ab_test('integer', 3).to_s
-  end
-
-  def integer_bingo
-    bingo! 'integer'
-    render :nothing => true
+    render :text => ab_test('integer', 3)
   end
 
   def range
-    render :text => ab_test('range', (1..3)).to_s
-  end
-
-  def range_bingo
-    bingo! 'range'
-    render :nothing => true
+    render :text => ab_test('range', (1..3))
   end
 
 
   # ab_choose specs.
+
   def array_choose
-    render :text => ab_choose('array', [1, 2, 3]).to_s
+    render :text => ab_choose('array', [1, 2, 3])
   end
 
 
   # specs for the helpers' behaviors with blocks
+
   def array_test_block
     ab_test 'array', [1, 2, 3] do |result|
-      render :text => result.to_s
+      render :text => result
     end
   end
 
   def array_choose_block
     ab_choose 'array', [1, 2, 3] do |result|
-      render :text => result.to_s
+      render :text => result
     end
   end
 
 
   # specs for multiple conversions
+
   def multiple_conversion
     number  = ab_test 'test_1', [1, 2, 3]
     boolean = ab_test 'test_2'
