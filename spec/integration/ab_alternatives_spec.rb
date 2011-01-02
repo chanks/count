3c393ae29d30c8fe42344f61e52b7f6e0864ceeb
Chris Hanks <christopher.m.hanks@gmail.com>
2011-01-02 14:49:56 -0800

    Don't bother with branch helper in a/b alternatives spec.

--- spec/integration/ab_alternatives_spec.rb
+++ spec/integration/ab_alternatives_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 # Spec the various types of alternatives that the ab_test helper can accept.
 
-branch :array, :boolean, :range, :hash, :integer do |test_type|
+[:array, :boolean, :range, :hash, :integer].each do |test_type|
   describe "An A/B #{test_type} test" do
     define_method :participate! do
       get "/controller_tests/#{test_type}"
@@ -19,29 +19,29 @@ branch :array, :boolean, :range, :hash, :integer do |test_type|
         100.times { clear_cookies; @results[participate!] += 1 }
       end
 
-      if test_type.boolean?
+      case test_type
+      when :boolean
         it "should return true and false each approximately 50% of the time" do
           [[true, false], [false, true]].should include @results.keys # can't sort - awkward
 
           @results.values.each { |value| value.should be_within(15).of(50) }
         end
-      end
 
-      if test_type.array? || test_type.range? || test_type.integer?
+      when :array, :range, :integer
         it "should return 1, 2 and 3 each approximately 33% of the time" do
           @results.keys.sort.should == [1, 2, 3]
 
           @results.values.each { |value| value.should be_within(15).of(33) }
         end
-      end
 
-      if test_type.hash?
+      when :hash
         it "should return 1 ~66% of the time and 2 ~33% of the time" do
           @results.keys.sort.should == [1, 2]
 
           @results[1].should be_within(15).of(66)
           @results[2].should be_within(15).of(33)
         end
+
       end
     end
   end
