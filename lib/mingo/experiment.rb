d1a15488e5dfd848f1eb9cd8165fefa9e0f338ea
Chris Hanks <christopher.m.hanks@gmail.com>
2011-01-02 11:57:17 -0800

    Oops - haven't used a conversion field in a little while.

--- lib/mingo/experiment.rb
+++ lib/mingo/experiment.rb
@@ -2,7 +2,7 @@ module Mingo
   class Experiment
     class << self
       def parse_all
-        fields = %w(experiment conversion alternative participant_count conversion_count)
+        fields = %w(experiment alternative participant_count conversion_count)
 
         alts = Mingo.collection.find({}, {:fields => fields}).group_by { |doc| doc['experiment'] }
 
