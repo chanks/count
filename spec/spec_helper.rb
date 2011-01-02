1f70cf943afb49bf627873683f6dc62253f926c6
Chris Hanks <christopher.m.hanks@gmail.com>
2011-01-02 14:54:51 -0800

    Just throw the spec helpers in spec_helper.rb. Not enough of them to warrant a support directory.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,3 +20,25 @@ RSpec.configure do |config|
 end
 
 Mingo.collection.drop
+
+# Helper to run a set of specs several times under different conditions.
+def branch(*args, &block)
+  args.map { |sym| ActiveSupport::StringInquirer.new sym.to_s }.each(&block)
+end
+
+def mingo_id
+  last_response.headers['mingo_id'].to_i
+end
+
+def populate(hash)
+  hash[:alternatives].each do |alternative, (participant_count, conversion_count)|
+    doc = { :test              => hash[:test],
+            :alternative       => alternative,
+            :participant_count => participant_count,
+            :participants      => [],
+            :conversion_count  => conversion_count,
+            :conversions       => [] }
+
+    Mingo.collection.insert(doc)
+  end
+end
