737341d3a6d3d66f257ef88252554d65972fffe0
Chris Hanks <christopher.m.hanks@gmail.com>
2011-01-04 17:56:28 -0800

    Remove some no-longer used spec helpers from the main suite.

--- spec/support/helpers.rb
+++ spec/support/helpers.rb
@@ -1,12 +1,3 @@
-# Helper to run a set of specs several times under different conditions.
-def branch(*args, &block)
-  args.map { |sym| ActiveSupport::StringInquirer.new sym.to_s }.each(&block)
-end
-
-def assigned_mingo_id
-  last_response.headers['mingo_id'].to_i
-end
-
 def populate(hash)
   hash[:alternatives].each do |alternative, (participant_count, conversion_count)|
     doc = { :_id               => hash[:test] + '/' + alternative.to_s,
